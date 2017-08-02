7cb1dd77bdada80d31287bd327a6f85a8562b730
LinhDH <danghoanglinh161994@gmail.com>
2017-08-02 16:49:39 +0700

    create migration database

---
+++ database/migrations/2017_08_02_093330_create_bids_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBidsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id');
+            $table->integer('user_got_id');
+            $table->integer('cost_begin');
+            $table->integer('cost_sell');
+            $table->integer('cost_max_current');
+            $table->timestamp('time_end');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bids');
+    }
+}
