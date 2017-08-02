5cd25ada037247ebc14da54f60beeb07305b8fbf
LinhDH <danghoanglinh161994@gmail.com>
2017-08-02 16:49:39 +0700

    create migration database

--- database/migrations/2017_08_02_092759_create_category_product_table.php
+++ database/migrations/2017_08_02_092759_create_category_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCategoryProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('category_product', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();;
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->tinyInteger('grant');
-            $table->boolean('is_active')->default(0);
+            $table->integer('category_id');
+            $table->integer('product_id');
             $table->boolean('is_deleted')->default(0);
-            $table->string('avatar');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('category_product');
     }
 }
--- database/migrations/2017_08_02_093830_create_user_bid_table.php
+++ database/migrations/2017_08_02_093830_create_user_bid_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserBidTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_bid', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();;
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->tinyInteger('grant');
-            $table->boolean('is_active')->default(0);
+            $table->integer('user_id');
+            $table->integer('bid_id');
+            $table->integer('cost');
             $table->boolean('is_deleted')->default(0);
-            $table->string('avatar');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_bid');
     }
 }
