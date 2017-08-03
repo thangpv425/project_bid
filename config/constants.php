9ff835a6946c7661d53f44b752f78231384b4378
truonghedspi <truong.hn.1994@gmail.com>
2017-08-03 09:58:40 +0700

    login, logout features

---
+++ config/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contain all constants in project
+ * @return Constant
+ */
+return [
+    'user_grant' => [
+        'admin' => '1',
+        'user' => '0'
+        // etc
+    ],
+
+    'user_status' => [
+        'active' => '1',
+        'de_active' => '0',
+    ]
+];
